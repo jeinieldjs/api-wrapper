31271ced5134f1dbfd06436ea9d6840320560970
jeinieldjs <silvestrejeiniel@gmail.com>
2024-01-23 14:18:17 +0800

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  namespace :api do
+    get '/entries', to: 'entries#index'
+    get '/random', to: 'random#index'
+    get '/categories', to: 'categories#index'
+    get '/health', to: 'health#index'
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
