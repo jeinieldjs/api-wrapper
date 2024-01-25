1e93dd3f667aa0138a5e1cf16e1bdb9c512b91a7
jeinieldjs <silvestrejeiniel@gmail.com>
2024-01-25 20:25:46 +0800

    Added tests for custom routes

--- test/controllers/api/entries_controller_test.rb
+++ test/controllers/api/entries_controller_test.rb
@@ -4,8 +4,20 @@ class Api::EntriesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test 'should get the list of all public API entries' do 
+  test 'should get the list of all entries' do 
     get api_entries_path
+    assert_response :success
+    assert_not_empty JSON.parse(response.body)['data']
+  end
+
+  test 'should get entries by category' do
+    get api_entries_path(category: 'art')
+    assert_response :success
+    assert_not_empty JSON.parse(response.body)['data']
+  end
+
+  test 'should get all entries that support HTTPS' do
+    get api_supports_https_path
     assert_response :success
     assert_not_empty JSON.parse(response.body)['data']
   end
